b13471072bf99785aa8b33f0bf53ca6766582274
pradeepdurai <pradeepdurai66@gmail.com>
2021-02-01 08:06:54 +0530

    Changes

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,6 +1,18 @@
 
 const User = require("../Models/user");
+const { check, validationResult } = require('express-validator');
+const user = require("../Models/user");
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+
+
 exports.signUp = (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        });
+    }
     var user = new User(req.body);
     user.save((err, user) => {
         if (err) {
@@ -8,10 +20,42 @@ exports.signUp = (req, res) => {
         }
         else {
             return res.json({
-                fullName : user.first_name+" - "+user.last_name,
-                email : user.email,
-                id : user._id
+                fullName: user.first_name + " - " + user.last_name,
+                email: user.email,
+                id: user._id
             });
         }
+    })
+}
+
+exports.signIn = (req, res) => {
+    const { email, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        });
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "Email does not exist"
+            })
+        }
+
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email And Password Does not Match"
+            })
+        }
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+        res.cookie("token", token, { expire: new Date() + 9999 });
+
+        // response to FED
+        const { _id, email, first_name, role } = user;
+        return res.json({
+            token, user: { _id, first_name, email, role }
+        })
     })
 }
--- Routes/auth.js
+++ Routes/auth.js
@@ -1,13 +1,22 @@
 
 
 const express = require("express");
-const { body, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 var router = express.Router();
 
-const {signUp} = require("../Controllers/auth");
+const {signUp,signIn} = require("../Controllers/auth");
 
-router.post("/signup",[body('email').isEmail()],signUp);
+router.post("/signup",[
+    check("first_name","First Name should be at least 3 letters").isLength({min : 3}),
+    check("email","email is required").isEmail(),
+    check("password","password should be at least 3 letters").isLength({min : 3})
+],signUp);
+
+router.post("/signin",[
+    check("email","email is required").isEmail(),
+    check("password","password should be at least 3 letters").isLength({min : 3})
+],signIn);
 
 
 module.exports = router;
