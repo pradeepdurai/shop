cb3e3926d2a15b2f0a1a7c9c712018e2750182fa
pradeepdurai <pradeepdurai66@gmail.com>
2021-02-08 12:06:20 +0530

    Changes

--- Controllers/user.js
+++ Controllers/user.js
@@ -3,11 +3,10 @@
 const User = require("../Models/user")
 
 exports.getUserById = (req, res, next, id) => {
-    User.findById(id).exec((err, user)=>{
-        if(!err ||!user)
-        {
+    User.findById(id).exec((err, user) => {
+        if (err || !user) {
             return res.status(400).json({
-                error : "User Not Found"
+                error: "User Not Found"
             });
         }
         req.profile = user;
@@ -15,8 +14,36 @@ exports.getUserById = (req, res, next, id) => {
     })
 }
 
-exports.getUser = (req, res) =>{
+exports.getUser = (req, res) => {
+    req.profile.salt = undefined;
+    req.profile.encry_password = undefined;
     return res.json(req.profile);
+}
+
+exports.allUsers = (req, res) => {
+    User.find().exec((err, user) => {
+        if (!user) {
+            return res.status(400).send("Users Not Found")
+        }
+        res.status(200).json(user);
+    })
+}
+
+exports.updateUser = (req, res) => {
+    User.findByIdAndUpdate(
+        { _id: req.profile._id },
+        { $set: req.body },
+        { new: true, useFindAndModify: false },
+        (err, user) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Your are not authorized to update the user"
+                })
+            }
+            user.salt = undefined;
+            user.encry_password = undefined;
+            return res.json(user);
+        })
 }
 
 
--- Routes/user.js
+++ Routes/user.js
@@ -2,13 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
-const {getUserById} = require("../Controllers/user");
+const {getUserById ,getUser,allUsers,updateUser} = require("../Controllers/user");
 
 const {isSignedIn, isAuthenticated,isAdmin} = require("../Controllers/auth");
-
+router.get("/user/get-all",allUsers);
 router.param("userId", getUserById);
+router.get("/user/:userId",isSignedIn, isAuthenticated ,getUser);
+router.put("/user/:userId",isSignedIn, isAuthenticated ,updateUser);
 
-router.get("/user/:userId",isSignedIn, isAuthenticated,isAdmin)
+
+
+
 
 
 module.exports = router;
